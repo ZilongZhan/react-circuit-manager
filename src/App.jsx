f3902f98db0dea4d22305d36a4582a96ac7c6ea5
AdriaCParra <Costaadria524@gmail.com>
2024-05-03 18:57:52 +0200

    Refactor App component, remove unused imports, and update BurgerMenu component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,6 @@
-import { Footer } from "./shared";
-import { Button, BurgerMenu } from "./shared";
-
 import "./App.css";
 import "./sanitize.css";
 
-function App() {
-  return (
-    <div className="app">
-      <BurgerMenu />
-      <Footer />
-    </div>
-  );
-}
+function App() {}
 
 export default App;
