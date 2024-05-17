20d61bb7e0f2164672b67aa0956ddb933248b65b
Núria Ruiz Font <nuriaruizfont@gmail.com>
2024-05-17 20:21:56 +0200

    working on form styles

--- src/components/ticketForm/Form.jsx
+++ src/components/ticketForm/Form.jsx
@@ -70,113 +70,122 @@ export const Form = () => {
 
   return (
     <form onSubmit={handleSubmit}>
-      <div className="title">
-        <button>
-          <img src="../../public/assets/icons/back.png" alt="" />
-        </button>
-
-        <h1>ISSUE </h1>
-        <h1> #ID</h1>
-        <h1>| dd-mm-aa</h1>
-      </div>
-      <div className="row">
-        <input className="col-12" type="text" placeholder="titol issue" />
-      </div>
-      <div className="row">
-        <div className="col-5 col-md-5">
-          <label htmlFor="department">Departaments:</label>
-          <select
-            id="department"
-            value={selectedDepartment}
-            onChange={handleDepartmentChange}
-          >
-            <option value="">Selecciona un Departament</option>
-            <option value="1">Department 1</option>
-            <option value="2">Department 2</option>
-            <option value="3">Department 3</option>
-          </select>
-        </div>
-        <div className="col-5 col-md-5">
-          <label htmlFor="zone">Zones:</label>
-          <select id="zone" value={selectedZone} onChange={handleZoneChange}>
-            <option value="">Selecciona una Zona</option>
-            <option value="1">Zone 1</option>
-            <option value="2">Zone 2</option>
-            <option value="3">Zone 3</option>
-          </select>
-        </div>
-        <div className="col-2 col-md-2">
-          <label>Priority:</label>
-          <div className="priorityButton">
-            <button
-              className={selectedPriority === "1" ? "selected" : ""}
-              onClick={() => handlePriorityChange("1")}
-            ></button>
-            <button
-              className={selectedPriority === "2" ? "selected" : ""}
-              onClick={() => handlePriorityChange("2")}
-            ></button>
-            <button
-              className={selectedPriority === "3" ? "selected" : ""}
-              onClick={() => handlePriorityChange("3")}
-            ></button>
+      <div className="container">
+        <div className="title row">
+          <div className="col-12">
+            <button>
+              <img src="../../public/assets/icons/back.png" alt="" />
+            </button>
+            <h1>ISSUE </h1>
+            <h1> #ID</h1>
+            <h1>| dd-mm-aa</h1>
           </div>
         </div>
-      </div>
-      <div className="row">
-        <div className="col-6 col-md-6">
-          <label htmlFor="details">Detalls:</label>
-          <textarea
-            id="details"
-            value={details}
-            onChange={handleDetailsChange}
-          />
+        <div className="row">
+          <input className="col-12 " type="text" placeholder="titol issue" />
         </div>
-        <div className="col-6 col-md-6">
-          <label htmlFor="images">Seleccionar imatges:</label>
-          <input
-            type="file"
-            id="images"
-            multiple
-            onChange={handleImageChange}
-            accept="image/*"
-          />
-          <ul>
-            {selectedImages.map((imageName, index) => (
-              <li key={index}>{imageName}</li>
-            ))}
-          </ul>
-        </div>
-      </div>
-      <div col-12>
-        <label htmlFor="periodificar">Periodificar:</label>
-        <input
-          type="checkbox"
-          id="periodificar"
-          checked={periodificar}
-          onChange={togglePeriodificar}
-        />
-        {periodificar && (
-          <div>
-            <label htmlFor="periodo">Periode:</label>
-            <select id="periodo" value={periodo} onChange={handlePeriodChange}>
-              <option value="días">Dies</option>
-              <option value="meses">Mesos</option>
-              <option value="años">Anys</option>
+        <div className="row">
+          <div className="col-5">
+            <label htmlFor="department">Departaments:</label>
+            <select
+              id="department"
+              value={selectedDepartment}
+              onChange={handleDepartmentChange}
+            >
+              <option value="">Selecciona un Departament</option>
+              <option value="1">Department 1</option>
+              <option value="2">Department 2</option>
+              <option value="3">Department 3</option>
             </select>
-            <span>
-              <button onClick={() => handleCantidadChange("decrease")}>
-                -
-              </button>
-              {cantidad}
-              <button onClick={() => handleCantidadChange("increase")}>
-                +
-              </button>
-            </span>
           </div>
-        )}
+          <div className="col-5 ">
+            <label htmlFor="zone">Zones:</label>
+            <select id="zone" value={selectedZone} onChange={handleZoneChange}>
+              <option value="">Selecciona una Zona</option>
+              <option value="1">Zone 1</option>
+              <option value="2">Zone 2</option>
+              <option value="3">Zone 3</option>
+            </select>
+          </div>
+          <div className="col-2 ">
+            <label>Priority:</label>
+            <div className="priorityButton">
+              <button
+                className={selectedPriority === "1" ? "selected" : ""}
+                onClick={() => handlePriorityChange("1")}
+              ></button>
+              <button
+                className={selectedPriority === "2" ? "selected" : ""}
+                onClick={() => handlePriorityChange("2")}
+              ></button>
+              <button
+                className={selectedPriority === "3" ? "selected" : ""}
+                onClick={() => handlePriorityChange("3")}
+              ></button>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-6">
+            <label htmlFor="details">Detalls:</label>
+            <textarea
+              id="details"
+              value={details}
+              onChange={handleDetailsChange}
+            />
+          </div>
+          <div className="col-6 ">
+            <label htmlFor="images">Seleccionar imatges:</label>
+            <input
+              type="file"
+              id="images"
+              multiple
+              onChange={handleImageChange}
+              accept="image/*"
+            />
+            <ul>
+              {selectedImages.map((imageName, index) => (
+                <li key={index}>{imageName}</li>
+              ))}
+            </ul>
+          </div>
+        </div>
+        <div className="row">
+          <div col-12>
+            <label htmlFor="periodificar">Periodificar:</label>
+            <input
+              type="checkbox"
+              id="periodificar"
+              checked={periodificar}
+              onChange={togglePeriodificar}
+            />
+            {periodificar && (
+              <div>
+                <label htmlFor="periodo">Periode:</label>
+                <select
+                  id="periodo"
+                  value={periodo}
+                  onChange={handlePeriodChange}
+                >
+                  <option value="días">Dies</option>
+                  <option value="meses">Mesos</option>
+                  <option value="años">Anys</option>
+                </select>
+                <span>
+                  <button onClick={() => handleCantidadChange("decrease")}>
+                    -
+                  </button>
+                  {cantidad}
+                  <button onClick={() => handleCantidadChange("increase")}>
+                    +
+                  </button>
+                </span>
+              </div>
+            )}
+          </div>
+        </div>
+        <button type="submit">Submit</button>
       </div>
-      <button type="submit">Submit</button>
     </form>
   );
 };
